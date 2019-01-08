67bdb94103725025080dcbda6027422e80d03cc1
amelielaurent <36437435+amelielaurent@users.noreply.github.com>
2019-01-08 11:53:45 +0100

    Type de bouton lors du click

--- app/src/main/java/com/amelielaurent38/digital/calculatrice/MainActivity.java
+++ app/src/main/java/com/amelielaurent38/digital/calculatrice/MainActivity.java
@@ -2,12 +2,48 @@ package com.amelielaurent38.digital.calculatrice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        TextView result = findViewById(R.id.tv_result);
+
+        if (!(v instanceof Button)) {
+            return;
+        }
+
+        if (v.getTag() == null) {
+            return;
+        }
+
+        String tag = v.getTag().toString();
+
+        Button button = (Button) v;
+        switch (tag) {
+            case "OP":
+                result.setText("");
+                break;
+
+            case "NB":
+                result.append(button.getText());
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
