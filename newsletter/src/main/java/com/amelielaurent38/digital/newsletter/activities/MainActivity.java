08c28ebd36bc91470e2b48bb26c1cfbdd37ac17c
amelielaurent <36437435+amelielaurent@users.noreply.github.com>
2019-01-10 15:15:47 +0100

    Newsletter article affichage

--- newsletter/src/main/java/com/amelielaurent38/digital/newsletter/activities/MainActivity.java
+++ newsletter/src/main/java/com/amelielaurent38/digital/newsletter/activities/MainActivity.java
@@ -4,20 +4,9 @@ import android.os.Bundle;
 
 import com.amelielaurent38.digital.newsletter.R;
 import com.amelielaurent38.digital.newsletter.fragments.ArticleListFragment;
-import com.amelielaurent38.digital.newsletter.models.Article;
-import com.amelielaurent38.digital.newsletter.models.ArticleResult;
-import com.amelielaurent38.digital.newsletter.network.ArticleService;
-import com.amelielaurent38.digital.newsletter.utils.Constants;
-
-import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,28 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://newsapi.org/v2/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ArticleService service = retrofit.create(ArticleService.class);
-
-        Call<ArticleResult> articles_new = service.listArticles("us", Constants.API_KEY);
-        articles_new.enqueue(new Callback<ArticleResult>() {
-            @Override
-            public void onResponse(Call<ArticleResult> call, Response<ArticleResult> response) {
-                response.body().getArticles();
-                System.out.println("Resultat Success" + response.body());
-            }
-
-            @Override
-            public void onFailure(Call<ArticleResult> call, Throwable t) {
-                System.out.println("Resultat Erreur" + t.getLocalizedMessage());
-            }
-        });
-
 
         showArticleList();
     }
--- newsletter/src/main/java/com/amelielaurent38/digital/newsletter/fragments/ArticleListFragment.java
+++ newsletter/src/main/java/com/amelielaurent38/digital/newsletter/fragments/ArticleListFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import com.amelielaurent38.digital.newsletter.R;
 import com.amelielaurent38.digital.newsletter.adapter.ArticleAdapter;
 import com.amelielaurent38.digital.newsletter.models.Article;
+import com.amelielaurent38.digital.newsletter.models.ArticleResult;
+import com.amelielaurent38.digital.newsletter.network.ArticleService;
+import com.amelielaurent38.digital.newsletter.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,24 +18,42 @@ import java.util.List;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ArticleListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.article_list_fragment, container, false);
 
-        List<Article> articles = new ArrayList<>();
-        articles.add(new Article("", "Terre", "La planete terre blblab"));
-        articles.add(new Article("", "Mars", "La planete mars"));
-        articles.add(new Article("", "Mercure", "La planete Mercure"));
-        articles.add(new Article("", "Planet 1", "blab ablaks nksnd dskjdklskmds sdkjdklklmdkksdsd"));
+        final RecyclerView recyclerView = view.findViewById(R.id.list_article);
+        
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        ArticleService service = retrofit.create(ArticleService.class);
 
-        RecyclerView recyclerView = view.findViewById(R.id.list_article);
-        ArticleAdapter adapter = new ArticleAdapter(articles);
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(adapter);
+        Call<ArticleResult> articles_new = service.listArticles("us", Constants.API_KEY);
+        articles_new.enqueue(new Callback<ArticleResult>() {
+            @Override
+            public void onResponse(Call<ArticleResult> call, Response<ArticleResult> response) {
+                System.out.println("Resultat Success" + response.body());
+                ArticleAdapter adapter = new ArticleAdapter(response.body().getArticles());
+                RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+                recyclerView.setLayoutManager(manager);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArticleResult> call, Throwable t) {
+                System.out.println("Resultat Erreur" + t.getLocalizedMessage());
+            }
+        });
 
         return view;
     }
